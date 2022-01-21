091eade294c91c53977a1e146010f8b790eb8f9f
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2022-01-21 13:17:29 -0800

    parse cookies

--- A1/a1.py
+++ A1/a1.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import re
 
 '''
 NOTE:
@@ -11,14 +12,40 @@ password protected: 401 http response
 
 PORT = 80
 
+def to_cookie_string(cookie):
+    s = f"cookie name: {cookie['name'].group().strip()}"
+    if cookie['expires']:
+        s += f", expires time: {cookie['expires'].group().strip()}"
+    if cookie['domain']:
+        s += f", domain name: {cookie['domain'].group().strip()}"
+    return s
+
+def get_cookies(response):
+    cookie_lines = filter(lambda line: line.startswith('Set-Cookie:'), response.split('\n'))
+    cookies = map(lambda cookie:
+                    {
+                        'name': re.search('(?<=Set-Cookie:)([^=]*)(?=\=)', cookie),
+                        'expires': re.search('(?<=expires=)([^;]*)', cookie),
+                        'domain': re.search('(?<=domain=)([^;]*)', cookie)
+                    }, cookie_lines)
+    return map(lambda cookie: to_cookie_string(cookie), cookies)
+
 def main(uri):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((uri, PORT))
     request = "GET /index.html HTTP/1.0\n\n"
     s.send(request.encode())
-    data = s.recv(10000)
-    print(data.decode())
+    response = s.recv(10000).decode()
     s.close()
+    
+    cookies = get_cookies(response)
+
+    # output
+    print("website:", uri)
+    print("1. Supports http2:")
+    print("2. List of Cookies:")
+    for cookie in cookies: print(cookie)
+    print("3. Password-protected:")
 
 if __name__ == "__main__":
     main(sys.argv[1])
