fe1c9671a17da76ae831d6162c651c8619759c83
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2022-01-21 22:04:22 -0800

    need to fix ssl http2 302 stuff

--- A1/a1.py
+++ A1/a1.py
@@ -1,5 +1,6 @@
-import socket
+from socket import *
 import sys
+import ssl
 import re
 
 '''
@@ -10,7 +11,7 @@ s =  ssl.SSLContext(protocol=ssl.PROTOCOL_TLSv1_3)
 password protected: 401 http response
 '''
 
-PORT = 80
+PORT = 443
 
 def to_cookie_string(cookie):
     s = f"cookie name: {cookie['name'].group().strip()}"
@@ -30,22 +31,46 @@ def get_cookies(response):
                     }, cookie_lines)
     return map(lambda cookie: to_cookie_string(cookie), cookies)
 
-def main(uri):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((uri, PORT))
-    request = "GET /index.html HTTP/1.0\n\n"
-    s.send(request.encode())
-    response = s.recv(10000).decode()
-    s.close()
-    
-    cookies = get_cookies(response)
+def check_401(response):
+    return any(filter(lambda line: line.startswith('HTTP/') and '401' in line, response.split('\n')))
 
-    # output
-    print("website:", uri)
-    print("1. Supports http2:")
-    print("2. List of Cookies:")
-    for cookie in cookies: print(cookie)
-    print("3. Password-protected:")
+def main(uri):
+    try:
+        ctx = ssl.create_default_context()
+        ctx.set_alpn_protocols(['h2'])
+
+        # s = socket(AF_INET, SOCK_STREAM)
+        # s.connect((uri, PORT))
+        # print("HERE:", is_http2_supported)
+        # request = "GET /index.html h2\n\n"
+        # s.send(request.encode())
+        # response = s.recv(10000).decode()
+        # print(response)
+        # s.close()
+
+        s = socket(AF_INET, SOCK_STREAM)
+        conn = ctx.wrap_socket(s, server_hostname=uri)
+        conn.connect((uri, PORT))
+        is_http2_supported = conn.selected_alpn_protocol()
+        print("HERE:", is_http2_supported)
+        request = "GET /index.html h2\n\n"
+        conn.send(request.encode())
+        response = conn.recv(10000).decode()
+        print(response)
+        conn.close()
+        
+        
+        cookies = get_cookies(response)
+        is_password_protected = check_401(response)
+
+        # output
+        print("website:", uri)
+        print("1. Supports http2:")
+        print("2. List of Cookies:")
+        for cookie in cookies: print(cookie)
+        print("3. Password-protected:", 'yes' if is_password_protected else 'no')
+    except error as e:
+        print(e)
 
 if __name__ == "__main__":
     main(sys.argv[1])
