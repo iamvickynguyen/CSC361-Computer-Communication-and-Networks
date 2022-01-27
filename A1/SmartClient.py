d19092bd00c7f3a2911ccbb284d5c8a18d60936d
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2022-01-26 21:38:29 -0800

    can redirect now

--- A1/SmartClient.py
+++ A1/SmartClient.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import ssl
 import re
+from urllib.parse import urlparse
 
 #https://python-hyper.org/projects/h2/en/stable/negotiating-http2.html
 
@@ -14,16 +15,71 @@ def is_http2_supported(url):
             return ssock.selected_alpn_protocol() == "h2"
 
 def get_status(response):
-    return int(re.search('(?<=HTTP/1.[0|1]\s*)(\d+)', response).group())
+    return int(re.search(r'(HTTP/1.[01]\s*)(\d+)', response).group(2))
 
-def connect():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    s.connect(("www.google.ca",80)) # Connect
-    request = "GET /index.html HTTP/1.0\n\n"
-    s.send(request.encode()) # Send request 
-    data = s.recv(10000).decode() # Get response
-    s.close()
-    print(data)
+def get_location(response):
+    new_location = urlparse(re.search(r'(Location:\s*)([^\r\n]*)', response).group(2))
+    location = new_location.netloc
+    path = new_location.path or '/'
+    is_https = new_location.scheme == "https"
+    return location, path, is_https
+
+def connect_http(host, path):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((host, 80))
+        request = f"GET {path} HTTP/1.1\nHost:{host}\r\n\r\n"
+        sock.sendall(request.encode())
+        response = sock.recv(10000).decode()
+        return response
+
+def connect_https(host, path):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((host, 443))
+        sock = ssl.wrap_socket(sock)
+        request = f"GET {path} HTTP/1.1\nHost:{host}\r\n\r\n"
+        sock.sendall(request.encode())
+        response = sock.recv(10000).decode() 
+        return response
+
+def connection(location):
+    response = connect_http(location, '/')
+    for i in range(5):
+        status = get_status(response)
+        print("STATUS:", status)
+        if status != 301 and status != 302:
+            return response
+
+        new_location, path, is_https = get_location(response)
+
+        if is_https:
+            response = connect_https(new_location, path)
+        else:
+            response = connect_http(new_location, path)
+
+
+def get_cookies(host):
+    return connection(host)
+
+# def test(host):
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+#         sock.connect((host, 80))
+#         request = f"GET / HTTP/1.1\nHost:{host}\nConnection: close\n\n"
+#         print(request)
+#         sock.sendall(request.encode())
+#         response = sock.recv(10000).decode()
+#         return response
+
+# def test2(host):
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     s.connect((host, 443))
+#     # s = ssl.wrap_socket(s, keyfile=None, certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
+#     s = ssl.wrap_socket(s)
+#     request = f"GET / HTTP/1.1\nHost:{host}\nConnection: close\n\n"
+#     print(request)
+#     s.sendall(request.encode())
+#     response = s.recv(10000).decode()
+#     return response
+
 
 def main():
     if len(sys.argv) != 2:
@@ -36,13 +92,16 @@ def main():
     print("1. Supports http2:", 'yes' if is_http2_supported(url) else 'no')
 
     # cookies
-    
     print("2. List of Cookies:")
+    # cookies = get_cookies(url)
+    response = get_cookies(url)
+    # connect_https('www.uvic.ca', '/')
+    # response = test2("www.uvic.ca")
+    print(response)
+    
 
     # password-protected
     print("3. Password-protected:", 'yes' if get_status(response) == 401 else 'no')
-
-    connect()
 
 if __name__ == "__main__":
     main()
