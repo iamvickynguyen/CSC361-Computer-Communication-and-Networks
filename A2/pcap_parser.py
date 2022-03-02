ec42a54e77c87e6fedde73445905d5b35dddba29
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2022-03-01 21:57:00 -0800

    kinda done parsing packet

---
+++ A2/pcap_parser.py
@@ -0,0 +1,58 @@
+import struct
+from packet_struct import *
+
+MAGIC_NUMBER_SAME = b'\xa1\xb2\xc3\xd4' # big endian
+MAGIC_NUMBER_SWAPPED = b'\xd4\xc3\xb2\xa1' # little endian
+
+class Global_Header:
+    def __init__(self, data):
+        self.byte_order = self._get_magic_number_order(data[:4])
+        self.thiszone = self._get_zone(data[9:13])
+
+    def _get_magic_number_order(self, bytes):
+        if bytes == MAGIC_NUMBER_SAME:
+            return 'big'
+        if bytes == MAGIC_NUMBER_SWAPPED:
+            return 'little'
+        raise ValueError("Invalid magic number")
+
+    def _get_zone(self, bytes):
+        return int.from_bytes(bytes, self.byte_order)
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
+
+class Packet_Header:
+    def __init__(self, data, byte_order='big'):
+        fmt = '>' if byte_order == 'big' else '<'
+        self.ts_sec, self.ts_usec, self.incl_len, self.orig_len = struct.unpack_from(fmt + 'IIII', data) # FIXME
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
+
+def parse_pkt_data(data):
+    protocol = data[14 + 9]
+    if protocol != 6: return None
+
+    ip_header = IP_Header()
+    ip_header.get_header_len(data[14:15])
+    ip_header_bytes = data[14:14 + ip_header.ip_header_len]
+    ip_header.get_total_len(ip_header_bytes[2:4])
+    ip_header.get_IP(ip_header_bytes[-8:-8+4], ip_header_bytes[-4:])
+    
+    tcp_header_bytes = data[14 + ip_header.ip_header_len:]
+    tcp_header = TCP_Header()
+    tcp_header.get_src_port(tcp_header_bytes[:2])
+    tcp_header.get_dst_port(tcp_header_bytes[2:4])
+    tcp_header.get_seq_num(tcp_header_bytes[4:8])
+    tcp_header.get_ack_num(tcp_header_bytes[8:12])
+    tcp_header.get_data_offset(tcp_header_bytes[12:13])
+    tcp_header.get_flags(tcp_header_bytes[13:14])
+    tcp_header.get_window_size(tcp_header_bytes[14:15], tcp_header_bytes[15:16])
+
+    pkt = packet()
+    pkt.set_ip_header(ip_header)
+    pkt.set_tcp_header(tcp_header)
+    return pkt
--- A2/tcp_analyzer.py
+++ A2/tcp_analyzer.py
@@ -1,11 +1,31 @@
 import sys
-from packet_struct import *
-from global_header import Global_Header
+from pcap_parser import Global_Header, Packet_Header, parse_pkt_data
 
 def main():
     with open(sys.argv[1], 'rb') as f:
-        global_header = Global_Header(f.read(24))
-        # print(global_header)
+        glob_header = Global_Header(f.read(24))
+        packets = []
+
+        while True:
+            next_bytes = f.read(16)
+
+            if not next_bytes:
+                break
+
+            p_header = Packet_Header(next_bytes, glob_header.byte_order)
+            packet = parse_pkt_data(f.read(p_header.incl_len))
+            if packet:
+                packets.append(packet)
+                print(packet)
+    
 
 if __name__ == "__main__":
     main()
+
+# 0:45
+# 0:57:13
+# 0:45:0
+# 0:45:0
+# 1:30:0
+# 0:45:0
+# https://github.com/faucetsdn/python3-ryu/blob/3bd6ce1cf4c739f6e86eecb2fdf98a13b10ba22a/ryu/lib/pcaplib.py
