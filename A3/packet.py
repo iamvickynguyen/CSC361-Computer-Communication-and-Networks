e14940b3fa4df663cc665c0941339cd900be76f3
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2022-03-27 21:45:45 -0700

    done IP Header

--- A3/packet.py
+++ A3/packet.py
@@ -12,7 +12,8 @@ class Global_Header:
             return 'big'
         if bytes == MAGIC_NUMBER_SWAPPED:
             return 'little'
-        raise ValueError("Invalid magic number")
+        # raise ValueError("Invalid magic number")
+        return 'little'
 
 class Packet_Header:
     def __init__(self, data, byte_order='big'):
@@ -26,6 +27,7 @@ class IP_Header:
         self.id = None #int
         self.flags = None #int
         self.fragment_offset = 0 #int
+        self.ttl = 0 #int
         self.protocol = None #int
         self.src_ip = None #str
         self.dst_ip = None #str
@@ -45,123 +47,128 @@ class IP_Header:
         self.id = struct.unpack('>H', buffer)[0]
 
     def set_fragment_offset(self, buffer):
-        pass
-        
-    def set_IP(self, buffer1, buffer2):
-        src_addr = struct.unpack('BBBB',buffer1)
-        dst_addr = struct.unpack('BBBB',buffer2)
+        result = struct.unpack('>H', buffer)[0]
+        self.flags = (result & 0b1111111111111111) >> 13
+        self.fragment_offset = (result & 0b1111111111111)
+
+    def set_ttl(self, buffer):
+        self.ttl = struct.unpack('B', buffer)[0]
+
+    def set_protocol(self, buffer):
+        self.protocol = struct.unpack('B', buffer)[0]
+
+    def set_src_ip(self, buffer):
+        src_addr = struct.unpack('BBBB',buffer)
         self.src_ip = str(src_addr[0])+'.'+str(src_addr[1])+'.'+str(src_addr[2])+'.'+str(src_addr[3])
+
+    def set_dst_ip(self, buffer):
+        dst_addr = struct.unpack('BBBB',buffer)
         self.dst_ip = str(dst_addr[0])+'.'+str(dst_addr[1])+'.'+str(dst_addr[2])+'.'+str(dst_addr[3])
- 
-
-class TCP_Header:    
-    def __init__(self):
-        self.src_port = 0
-        self.dst_port = 0
-        self.seq_num = 0
-        self.ack_num = 0
-        self.data_offset = 0
-        self.flags = {}
-        self.window_size =0
         
-    def set_src_port(self, buffer):
-        num1 = ((buffer[0]&240)>>4)*16*16*16
-        num2 = (buffer[0]&15)*16*16
-        num3 = ((buffer[1]&240)>>4)*16
-        num4 = (buffer[1]&15)
-        self.src_port = num1+num2+num3+num4
-    
-    def set_dst_port(self, buffer):
-        num1 = ((buffer[0]&240)>>4)*16*16*16
-        num2 = (buffer[0]&15)*16*16
-        num3 = ((buffer[1]&240)>>4)*16
-        num4 = (buffer[1]&15)
-        self.dst_port = num1+num2+num3+num4
-    
-    def set_seq_num(self, buffer):
-        self.seq_num = struct.unpack(">I",buffer)[0]
-    
-    def set_ack_num(self, buffer):
-        self.ack_num = struct.unpack('>I',buffer)[0]
 
-    def set_flags(self, buffer):
-        value = struct.unpack("B",buffer)[0]
-        self.flags["ACK"] = (value & 16)>>4
-        self.flags["RST"] = (value & 4)>>2
-        self.flags["SYN"] = (value & 2)>>1
-        self.flags["FIN"] = value & 1
-
-    def set_window_size(self, buffer1, buffer2):
-        buffer = buffer2+buffer1
-        self.window_size = struct.unpack('H',buffer)[0]
-
-    def set_data_offset(self, buffer):
-        value = struct.unpack("B",buffer)[0]
-        self.data_offset = ((value & 240)>>4)*4
-   
-
-class packet():
+class UDP_Header:
     def __init__(self):
-        self.IP_header = IP_Header()
-        self.TCP_header = TCP_Header()
+        self.src_port = None #int
+        self.dst_port = None #int
+        self.udp_length = None #int
+        self.checksum = None #int
+
+    def set_src_port(self, buffer):
+        self.src_port = struct.unpack('>H', buffer)[0]
+
+    def set_dst_port(self, buffer):
+        self.dst_port = struct.unpack('>H', buffer)[0]
+
+    def set_udp_length(self, buffer):
+        self.udp_length = struct.unpack('>H', buffer)[0]
+
+    def set_checksum(self, buffer):
+        self.checksum = struct.unpack('>H', buffer)[0]
+
+
+class ICMP_Header:
+    def __init__(self):
+        self.type = None #int
+        self.code = None #int
+        # TODO
+
+    def set_type(self, buffer):
+        self.type = struct.unpack('B', buffer)[0]
+
+    def set_code(self, buffer):
+        self.code = struct.unpack('B', buffer)[0]
+
+    #TODO
+
+
+class Packet:
+    def __init__(self):
+        self.ip_header = IP_Header()
+        self.udp_header = UDP_Header()
+        self.icmp_header = ICMP_Header()
         self.timestamp = 0
-        self.packet_No =0
-        self.RTT_flag = False
+        self.packet_no = 0
+
+    def set_ip_header(self, ip_header):
+        self.ip_header = ip_header
+
+    def set_udp_header(self, udp_header):
+        self.udp_header = udp_header
+
+    def set_icmp_header(self, icmp_header):
+        self.icmp_header = icmp_header
 
     def set_timestamp(self, seconds, microseconds, orig_time):
         self.timestamp = round(seconds+microseconds*0.000001-orig_time,6)
 
-    def set_packet_number(self, number):
-        self.packet_No = number
-
-    def set_ip_header(self, ip_header):
-        self.IP_header = ip_header
-
-    def set_tcp_header(self, tcp_header):
-        self.TCP_header = tcp_header
-
-    def get_flags(self):
-        return self.TCP_header.flags
-
-    def get_data_bytes(self):
-        return self.IP_header.total_len - self.IP_header.get_header_len() - self.TCP_header.data_offset
-
-    def get_window_size(self):
-        return self.TCP_header.window_size
-
-    def get_seq_number(self):
-        return self.TCP_header.seq_num
-
-    def get_ack_number(self):
-        return self.TCP_header.ack_num
+    def set_packet_number(self, packet_no):
+        self.packet_no = packet_no
 
     def __str__(self):
-        return str(self.__class__) + ": <IP HEADER>" + str(self.IP_header.__dict__) + ": <TCP HEADER>" + str(self.TCP_header.__dict__)
+        return str(self.__class__) + ": <IP>" + str(self.ip_header.__dict__) + ": <UDP>" + str(self.udp_header.__dict__) + ": <ICMP>" + str(self.icmp_header.__dict__)
 
 
-def parse_pkt_data(data, number: int, p_header: Packet_Header) -> packet:
-    protocol = data[14 + 9]
-    if protocol != 17 and protocol != 1: return None
-
+def parse_ip_header(data) -> IP_Header:
     ip_header = IP_Header()
-    ip_header.set_header_len(data[14:15])
-    ip_header_bytes = data[14:14 + ip_header.ihl]
-    ip_header.set_total_len(ip_header_bytes[2:4])
-    ip_header.set_IP(ip_header_bytes[-8:-8+4], ip_header_bytes[-4:])
-    
-    tcp_header_bytes = data[14 + ip_header.ihl:]
-    tcp_header = TCP_Header()
-    tcp_header.set_src_port(tcp_header_bytes[:2])
-    tcp_header.set_dst_port(tcp_header_bytes[2:4])
-    tcp_header.set_seq_num(tcp_header_bytes[4:8])
-    tcp_header.set_ack_num(tcp_header_bytes[8:12])
-    tcp_header.set_data_offset(tcp_header_bytes[12:13])
-    tcp_header.set_flags(tcp_header_bytes[13:14])
-    tcp_header.set_window_size(tcp_header_bytes[14:15], tcp_header_bytes[15:16])
+    ip_header.set_ihl(data[:1])
+    ip_header.set_total_len(data[2:4])
+    ip_header.set_id(data[4:6])
+    ip_header.set_fragment_offset(data[6:8])
+    ip_header.set_ttl(data[8:9])
+    ip_header.set_protocol(data[9:10])
+    ip_header.set_src_ip(data[12:16])
+    ip_header.set_dst_ip(data[16:])
+    return ip_header
 
-    pkt = packet()
-    pkt.set_ip_header(ip_header)
-    pkt.set_tcp_header(tcp_header)
-    pkt.set_packet_number(number)
-    pkt.set_timestamp(p_header.ts_sec, p_header.ts_usec, 0)
-    return pkt
+def parse_udp_header(data) -> UDP_Header:
+    udp_header = UDP_Header()
+    udp_header.set_src_port(data[:2])
+    udp_header.set_dst_port(data[2:4])
+    udp_header.set_udp_length(data[4:6])
+    udp_header.set_checksum(data[6:])
+    return udp_header
+
+# TODO
+def parse_icmp_header(data) -> ICMP_Header:
+    icmp_header = ICMP_Header()
+    icmp_header.set_type(data[:1])
+    icmp_header.set_code(data[1:2])
+    return icmp_header
+
+def get_packet(data, pkt_number: int, pkt_header: Packet_Header) -> Packet:
+    packet = Packet()
+    packet.set_packet_number(pkt_number)
+    packet.set_timestamp(pkt_header.ts_sec, pkt_header.ts_usec, 0)
+
+    ip_header = parse_ip_header(data[14:14 + 20])
+    packet.set_ip_header(ip_header)
+
+    if ip_header.protocol == 1:
+        icmp_header = parse_icmp_header(data[20:28])
+        packet.set_icmp_header(icmp_header)
+        return packet
+    elif ip_header.protocol == 17:
+        udp_header = parse_udp_header(data[20:28])
+        packet.set_udp_header(udp_header)
+        return packet
+    return None
--- A3/traceroute.py
+++ A3/traceroute.py
@@ -1,31 +1,21 @@
 import sys
-import struct
-# from parser import Global_Header, Packet_Header
-
+from packet import Global_Header, Packet_Header, get_packet
 
 def main():
     with open(sys.argv[1], 'rb') as f:
-        # glob_header = Global_Header(f.read(24))
-        f.read(24)
+        glob_header = Global_Header(f.read(24))
+        packets = []
 
-        # while True:
-        #     next_bytes = f.read(16)
-        #     if not next_bytes: break
-        #     p_header = Packet_Header(next_bytes, glob_header.byte_order)
-        #     packet = parse_pkt_data(f.read(p_header.incl_len), len(packets) + 1, p_header)
-        #     if packet: packets.append(packet)
+        while True:
+            next_bytes = f.read(16)
+            if not next_bytes: break
+            pkt_header = Packet_Header(next_bytes, glob_header.byte_order)
+            pkt_data = f.read(pkt_header.incl_len)
+            packet = get_packet(pkt_data, len(packets) + 1, pkt_header)
+            if packet: packets.append(packet)
 
-        for i in range(2):
-            test = f.read(16)
-            a,b,c,d = struct.unpack_from('<IIII', test)
-            f.read(c)
-        
-        test = f.read(16)
-        print(test)
-        a,b,c,d = struct.unpack_from('<IIII', test)
-        data = f.read(c)
-        data = data[14:] # rm ethernet
-        print(data[4:8])
+        for p in packets:
+            print(p)
 
 if __name__ == "__main__":
     main()
