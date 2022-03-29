966b9edf68936276633c08a26a7005ff6d69d47e
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2022-03-28 17:19:09 -0700

    can get src_ip and dst_ip now

---
+++ A3/report.py
@@ -0,0 +1,4 @@
+def output_report(src_ip, dst_ip, src_packets, dst_packets):
+    print(f"The IP address of the source node: {src_ip}")
+    print(f"The IP address of ultimate destination node: {dst_ip}")
+    
--- A3/traceroute.py
+++ A3/traceroute.py
@@ -1,10 +1,13 @@
 import sys
 from packet import Global_Header, Packet_Header, get_packet
+from report import output_report
 
 def main():
+    packets = []
     with open(sys.argv[1], 'rb') as f:
         glob_header = Global_Header(f.read(24))
-        packets = []
+        src, dst = [], []
+        src_ip, dst_ip = None, None
 
         while True:
             next_bytes = f.read(16)
@@ -12,7 +15,15 @@ def main():
             pkt_header = Packet_Header(next_bytes, glob_header.byte_order)
             pkt_data = f.read(pkt_header.incl_len)
             packet = get_packet(pkt_data, len(packets) + 1, pkt_header)
-            if packet: packets.append(packet)
+            if packet:
+                if packet.udp_header:
+                    src.append(packet)
+                    if packet.ip_header.ttl == 1:
+                        src_ip = packet.ip_header.src_ip
+                        dst_ip = packet.ip_header.dst_ip
+                else: dst.append(packet)
+        
+    output_report(src_ip, dst_ip, src, dst)
 
 if __name__ == "__main__":
     main()
